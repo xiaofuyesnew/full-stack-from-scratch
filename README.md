542b12ae33df699497f6a5c8a47a709c4196052e
xiaofuyesnew <xiaofuyesnew@qq.com>
2023-11-16 18:27:18 +0800

    feat: server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Full-stack From Scratch
 
 > Do some full-stack coding from scratch
+
+https://shipfa.st/
--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,7 +1,10 @@
+import process from 'node:process'
 import Koa from 'koa'
 
 // create app
 const app = new Koa()
 
 // listen port 3000
-app.listen(3000)
+app.listen(process.env.PORT, () => {
+  console.log(process.env.PORT)
+})
